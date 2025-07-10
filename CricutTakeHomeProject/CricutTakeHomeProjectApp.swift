6589afd40be598c0dda8baf5e19001e46a139fd8
Madhav Gadve <maddymadhav1994@gmail.com>
2025-07-10 17:26:18 -0500

    Initial Commit

---
+++ CricutTakeHomeProject/CricutTakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CricutTakeHomeProjectApp.swift
+//  CricutTakeHomeProject
+//
+//  Created by Madhav Gadve on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CricutTakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
